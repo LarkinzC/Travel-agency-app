566ceca06a52c74f9643163526803e0680ff698e
Larkin Campbell <Larkincampbell24@gmail.com>
2025-06-11 19:47:13 -0700

    img and title added

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -4,7 +4,10 @@ import { Link } from 'react-router'
 const NavItems = () => {
   return (
     <section className='nav-items'>
-      <Link to='/' className='link-logo'/>
+      <Link to='/' className='link-logo'> 
+        <img src='/assets/icons/logo.svg' alt='logo' className='size-[30px]'/>
+        <h1>TourVisto</h1>
+      </Link>
     </section>
   )
 }
