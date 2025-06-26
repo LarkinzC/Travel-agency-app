b43dbeee5c0950a621d888e1c4b0671146dcb729
Larkin Campbell <Larkincampbell24@gmail.com>
2025-06-25 19:16:32 -0700

    sidebar functionality integrated

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,6 +1,9 @@
+import type { SidebarComponent } from "@syncfusion/ej2-react-navigations"
 import { Link } from "react-router"
 
 const MobileSidebar = () => {
+    let sidebar: SidebarComponent
+
   return (
     <div className="mobile-sidebar wrapper">
         <header>
@@ -8,6 +11,7 @@ const MobileSidebar = () => {
                 <img src="/assets/icons/logo.svg" alt="Logo" className="size-[30px]" />
                 <h1>Tourvisto</h1>
             </Link>
+            <button onClick={() => sidebar.toggle}></button>
         </header>
         </div>
   )
