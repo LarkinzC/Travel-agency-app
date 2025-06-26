b27848dc636db0bd4fb1a03553aa4273fc0aacd2
Larkin Campbell <Larkincampbell24@gmail.com>
2025-06-25 19:14:30 -0700

    logo for mobile and sidebar creation

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarComponent } from '@syncfusion/ej2-react-navigations'
+import { MobileSidebar } from 'components'
 import NavItems from 'components/NavItems'
 import React from 'react'
 import {Outlet} from 'react-router'
@@ -6,12 +7,14 @@ import {Outlet} from 'react-router'
 const AdminLayout = () => {
   return (
     <div className='admin-layout'>
-      MobileSidebar
+      <MobileSidebar />
+
       <aside className='w-full max-w-[270px] hidden lg:block'>
         <SidebarComponent width={270} enableGestures={false}>
           <NavItems />
         </SidebarComponent>
       </aside>
+      
       <aside className='children'>
           <Outlet />
       </aside>
--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router"
 
 const MobileSidebar = () => {
   return (
-    <div>MobileSidebar</div>
+    <div className="mobile-sidebar wrapper">
+        <header>
+            <Link to='/'>
+                <img src="/assets/icons/logo.svg" alt="Logo" className="size-[30px]" />
+                <h1>Tourvisto</h1>
+            </Link>
+        </header>
+        </div>
   )
 }
 
